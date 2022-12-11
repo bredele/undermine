37a24761853caae5edfe8237b6beb6c9d0993d81
Olivier Wietrich <olivier.wietrich@gmail.com>
2022-12-11 11:08:16 -0600

    feat: Remove module cache

--- index.js
+++ index.js
@@ -4,24 +4,47 @@ const Module = require('module')
 // cache default module require
 const load = Module.prototype.require
 
-// dependency tree
-const dependencyTree = {}
-
 // extend node module require
 Module.prototype.require = function (path) {
-  const content = load(path)
-  dependencyTree[path] = []
+  const content = load.call(this, path)
+  addChildDependencies(Module._resolveFilename(path, this))
   return content
 }
 
+// dependency tree
+const dependencyTree = {}
+
 /**
- * Invalidate module with given filename.
+ * Add child dependencies for a given module.
+ * A dependency is a module that will need to be reseted whenever the parent
+ * module (with the fiven `modFilename]`) changes.
+ *
+ * @param {String} modFilename
+ * @private
+ */ 
+
+const addChildDependencies = (modFilename) => {
+  const { children } = require.cache[modFilename]
+  for (const { filename } of children) {
+    const dep = dependencyTree[filename] = dependencyTree[filename] || [] 
+    if (!dep.includes(modFilename)) dep.push(modFilename)
+  }
+}
+
+/**
+ * Invalidate module cache with given filename.
+ *
+ * @param {String} filename (absolute filename path)
+ * @public
  */
 
 module.exports = (filename) => {
   const deps = dependencyTree[filename]
-  for (const dep of deps) {
-    delete require.cache[dep]
+  if (deps) {
+    for (const dep of deps) {
+      delete require.cache[dep]
+    }
   }
   delete require.cache[filename]
 }
+
--- test/undermine.test.js
+++ test/undermine.test.js
@@ -5,19 +5,34 @@ const invalidate = require('..')
 
 test('should reset module', async (assert) => {
   assert.plan(2)
-  const path = join(__dirname, 'mod-test.js')
-  await writeFile(path, `
+  const a = join(__dirname, 'a.js')
+  await writeFile(a, `
     module.exports = 'hello'
   `)
-  assert.equal(require(path), 'hello')
-  await writeFile(path, `
+  assert.equal(require(a), 'hello')
+  await writeFile(a, `
     module.exports = 'world'
   `)
-  invalidate(path)
-  assert.equal(require(path), 'world')
+  invalidate(a)
+  assert.equal(require(a), 'world')
 })
 
-/*test('should reset module dependencies', (assert) => {
-  assert.plan(1)
-})*/
+test('should reset module dependencies', async (assert) => {
+  assert.plan(2)
+  const c = join(__dirname, 'c.js')
+  await writeFile(c, `
+    module.exports = 'hello'
+  `)
+  const d = join(__dirname, 'd.js')
+  await writeFile(d, `
+    const c = require('./c')
+    module.exports = c
+  `)
+  assert.equal(require(d), 'hello')
+  invalidate(c)
+  await writeFile(c, `
+    module.exports = 'world'
+  `)
+  assert.equal(require(d), 'world')
+})
 
